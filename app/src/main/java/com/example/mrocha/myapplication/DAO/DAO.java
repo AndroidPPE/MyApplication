834af097d7bae7babed4a188824ab030e876f73c
gwendoline boisson <gwendoline.boisson@gmail.com>
2018-04-03 10:57:39 +0200

    com gwen

---
+++ app/src/main/java/com/example/mrocha/myapplication/DAO/DAO.java
@@ -0,0 +1,50 @@
+package com.example.mrocha.myapplication.DAO;
+
+/*
+    ###############################################
+    Nom de la classe : DAO
+    Ajouter par : Gwen
+    Date d'ajout : 03/04/2018
+    Version d'ajout : V1
+    Description : Class DAO
+    ###############################################
+     */
+
+public abstract class DAO<T> {
+
+    /*
+    ###############################################
+    Méthode insert
+    Ajouter par : Gwen
+    Date d'ajout : 03/04/2018
+    Version d'ajout : V1
+    Description : Permet de créer une entrée dans la base de données par rapport à un objet
+    ###############################################
+     */
+
+    public abstract boolean insert(T obj);
+
+    /*
+    ###############################################
+    Méthode update
+    Ajouter par : Gwen
+    Date d'ajout : 03/04/2018
+    Version d'ajout : V1
+    Description : Permet de mettre à jour les données d'une entrée dans la base
+    ###############################################
+     */
+
+    public abstract boolean update(T obj);
+
+    /*
+    ###############################################
+    Méthode delete
+    Ajouter par : Gwen
+    Date d'ajout : 03/04/2018
+    Version d'ajout : V1
+    Description : Permet la suppression d'une entrée de la base
+    ###############################################
+     */
+
+    public abstract boolean delete(T obj);
+}
---
+++ app/src/main/java/com/example/mrocha/myapplication/DAO/MonSQLiteOH.java
@@ -0,0 +1,26 @@
+package com.example.mrocha.myapplication.DAO;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/*
+    ###############################################
+    Nom de la classe : MonSQLiteOH
+    Ajouter par : Gwen
+    Date d'ajout : 03/04/2018
+    Version d'ajout : V1
+    Description : Class MonSQLiteOH
+    ###############################################
+     */
+
+public class MonSQLiteOH extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
